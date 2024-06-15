d67c4443942cbafa67cbd70a46ea68cbf2652bab
Sahil Gidwani <79597840+sahil-gidwani@users.noreply.github.com>
2024-06-16 00:21:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,31 @@ import rembg
 import io
 import zipfile
 
-rembg.bg.get_model('u2net')
+# URL to the model
+MODEL_URL = "https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx"
+MODEL_PATH = "./models/u2net.onnx"
 
-def remove_background(image):
+# Function to download the model if it doesn't exist
+def download_model(url, model_path):
+    if not os.path.exists(model_path):
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+        response = requests.get(url)
+        with open(model_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded model to {model_path}")
+
+# Function to remove background
+def remove_background(image, model_path):
     image_np = np.array(image)
-    result = rembg.remove(image_np)
+    session = rembg.new_session(model_name='u2net', model_path=model_path)
+    result = rembg.remove(image_np, session=session)
     result_image = Image.fromarray(result)
     white_bg_image = Image.new("RGBA", result_image.size, "WHITE")
     white_bg_image.paste(result_image, (0, 0), result_image)
     return white_bg_image.convert("RGB")
+
+# Download the model if necessary
+download_model(MODEL_URL, MODEL_PATH)
 
 st.title("Bulk Background Remover and White Background Adder")
 
